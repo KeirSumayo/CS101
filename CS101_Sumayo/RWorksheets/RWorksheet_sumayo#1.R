f38af3fb6fe4ffaf7c60a3c0792be058884d50f2
KeirSumayo <keir.sumayo@students.isatu.edu.ph>
2025-09-22 02:57:29 +0000

    Edited a file

--- CS101_Sumayo/RWorksheets/RWorksheet_sumayo#1.R
+++ CS101_Sumayo/RWorksheets/RWorksheet_sumayo#1.R
@@ -1,12 +1,15 @@
+#1
 age <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 51, 35, 24, 33, 41)
 
 length(age)
 age
 
+#2
 recip_age <- 1/age
 
 recip_age
 
+#3
 new_age <- c(age, 0, age)
 new_age
 
@@ -14,22 +17,27 @@ new_age
 #then printed 0 as the second parameter, 
 #and called again the "age" as the third parameter.
 
+#4
 sort(age)
 
+#5
 min(age)
 max(age)
 
+#6
 data <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, 2.7)
 
 length(data)
 data
 
+#7
 double_vector <- data*2
 
 double_vector
 
 #It took the value of a vector "data" and multiplied it in 2
 
+#8
 int <- 1:100
 num <- 20:60
 
@@ -48,14 +56,17 @@ display_length
 find_max <- tail(seq_of_int, 10)
 find_max
 
+#9
 vec_int <- c(1:100)
 
 result <- Filter(function(i) { all (i %% c(3,5,7) !=0) }, vec_int)
 result
 
+#10
 backward_seq <- 100:1
 backward_seq
 
+#11
 natural_num <- 1:24
 result <- natural_num[natural_num %% 3 == 0 | natural_num %% 5 == 0]
 result
@@ -63,6 +74,16 @@ result
 display_length <- c(length(backward_seq), length(result))
 display_length
 
+#12
+{x <- 0
++ x + 5 
+}
+
+#{x <- 0 + x + 5 +}, this is supposed to be the code, the output of this code is 5, the + signs pop
+# automatically because R thinks the line isn't finished. To my understanding,
+# Curly braces is a way to bundle multiple statements so R can read it as one unit.
+
+#13
 score <- c(72, 86, 92, 63, 88, 89, 91, 92, 75, 75, 77)
 score[2]
 score[3]
